3dcbdc387aab09e2f22f9aea2bdc88963989ef8c
PHGA <phga@cesar.school>
2019-09-21 16:54:23 -0300

    Updates

---
+++ precache-manifest.110e43572e59b8f1849913961c53e525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985f0906123d64bff86e24532ceaa9fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84bb6816c005cf60d74a",
+    "url": "/portfolio/static/css/main.c22ad210.chunk.css"
+  },
+  {
+    "revision": "196d005763a7303c6077",
+    "url": "/portfolio/static/js/2.b55ae453.chunk.js"
+  },
+  {
+    "revision": "84bb6816c005cf60d74a",
+    "url": "/portfolio/static/js/main.22d801b1.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  },
+  {
+    "revision": "8ba11c6297186b9f48f2098b81198d1b",
+    "url": "/portfolio/static/media/eu.8ba11c62.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
